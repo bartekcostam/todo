e274c1aaadf8add17a99c66f99d4673e28d6bcab
Bartlomiej Wydmuch <bartekoffice1@gmail.com>
2023-06-11 15:16:28 +0200

    set the express logic to provide endpoint

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+let todoList = [];
+
+app.use(express.json());
+
+app.get('/todos', (req, res) => {
+  res.json(todoList);
+});
+
+app.post('/todos', (req, res) => {
+  const { note } = req.body;
+  todoList.push(note);
+  res.status(201).json({ message: 'Note added successfully', note });
+});
+
+app.put('/todos/:id', (req, res) => {
+  const { id } = req.params;
+  const { note } = req.body;
+  if (id >= 0 && id < todoList.length) {
+    todoList[id] = note;
+    res.json({ message: 'Note updated successfully', note });
+  } else {
+    res.status(404).json({ message: 'Note not found' });
+  }
+});
+
+app.delete('/todos/:id', (req, res) => {
+  const { id } = req.params;
+  if (id >= 0 && id < todoList.length) {
+    const removedNote = todoList.splice(id, 1);
+    res.json({ message: 'Note deleted successfully', note: removedNote });
+  } else {
+    res.status(404).json({ message: 'Note not found' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
