4cfe6f3d25ead54b63c1e4a3abaad56c47f8a648
Bartlomiej Wydmuch <bartekoffice1@gmail.com>
2023-06-11 20:17:23 +0200

    come back with 3000

--- app.js
+++ app.js
@@ -111,6 +111,6 @@ app.put('/todos/:id', (req, res) => {
         });
 });
 
-app.listen(8080, () => {
-    console.log('Server is running on port 8080');
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
