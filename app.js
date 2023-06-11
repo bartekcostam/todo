eb7a313f1b767c564ad7ec46777dc0db675a5099
Bartlomiej Wydmuch <bartekoffice1@gmail.com>
2023-06-11 17:05:36 +0200

    fixing all errors and proper testing

--- app.js
+++ app.js
@@ -1,79 +1,114 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs').promises; 
-const util = require('util');
+const fs = require('fs').promises;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-let data;
-fs.readFile('data.json')
-    .then(fileData => {
-        data = JSON.parse(fileData);
-    })
-    .catch(err => {
-        if (err.code === 'ENOENT') {
-            data = [];
-        } else {
-            throw err;
-        }
-    });
+function getData() {
+    return fs.readFile('data.json', 'utf8')
+        .then(fileData => {
+            try {
+                // If the file is empty, return an empty array
+                if (fileData === '') {
+                    return [];
+                }
 
-function saveData() {
+                // Otherwise, parse the JSON data
+                return JSON.parse(fileData);
+            } catch (err) {
+                console.error('Failed to parse JSON data:', err);
+                throw err;
+            }
+        })
+        .catch(err => {
+            if (err.code === 'ENOENT') {
+                return [];
+            } else {
+                throw err;
+            }
+        });
+}
+
+
+function saveData(data) {
     return fs.writeFile('data.json', JSON.stringify(data));
 }
 
 app.get('/todos', (req, res) => {
-    res.json(data);
+    getData()
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.sendStatus(500);
+        });
 });
 
 app.post('/todos', (req, res) => {
-    const task = req.body;
-    task.id = Date.now();  
-    task.date = new Date().toISOString();
-    data.push(task);
-    saveData()
-        .then(() => {
-            res.json(task);
+    getData()
+        .then(data => {
+            const task = req.body;
+            task.id = Date.now(); // Here is where the id is assigned
+            task.date = new Date().toISOString();
+            data.push(task);
+            return saveData(data)
+                .then(() => {
+                    res.json(task); // Include the assigned task ID in the response
+                });
         })
         .catch(err => {
-            res.sendStatus(500);
+            console.error(err);
+            res.status(500).json({ error: 'Internal server error' });
         });
+        
 });
+
 
 app.delete('/todos/:id', (req, res) => {
-    const index = data.findIndex(task => task.id == req.params.id);
-    if (index !== -1) {
-        data.splice(index, 1);
-        saveData()
-            .then(() => {
-                res.sendStatus(200);
-            })
-            .catch(err => {
-                res.sendStatus(500);
-            });
-    } else {
-        res.sendStatus(404);
-    }
-});
-
-app.put('/todos/:id', (req, res) => {
-    const index = data.findIndex(task => task.id == req.params.id);
-    if (index !== -1) {
-        const task = req.body;
-        task.date = new Date().toISOString();
-        data[index] = task;
-        saveData()
-            .then(() => {
-            res.json(task);
+    const id = req.params.id;
+    getData()
+        .then(data => {
+            const index = data.findIndex(task => task.id == id);
+            if (index !== -1) {
+                data.splice(index, 1);
+                return saveData(data)
+                    .then(() => {
+                        res.sendStatus(200);
+                    });
+            } else {
+                res.status(404).json({ error: 'Task not found' });
+            }
         })
         .catch(err => {
-            res.sendStatus(500);
+            res.status(500).json({ error: 'Internal server error' });
         });
-    } else {
-        res.sendStatus(404);
-    }
+});
+
+
+
+
+app.put('/todos/:id', (req, res) => {
+    getData()
+        .then(data => {
+            const index = data.findIndex(task => task.id == req.params.id);
+            if (index !== -1) {
+                const task = req.body;
+                task.date = new Date().toISOString();
+                data[index] = task;
+                return saveData(data)
+                    .then(() => {
+                        res.json(task); // Move this line here
+                    });
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'Internal server error' });
+        });
 });
 
 app.listen(3000, () => {
